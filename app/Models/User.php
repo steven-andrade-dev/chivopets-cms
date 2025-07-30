974d665e599580a3d2262b8340cc83fc83503600
steve-andrade-dev <steven.andrade@goes.gob.sv>
2025-07-30 15:23:04 -0600

    MOD user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model

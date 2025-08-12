1bf3aad6147a45fba06c72cd6e3f5073364c9f4b
steve-andrade-dev <steven.andrade@goes.gob.sv>
2025-08-12 11:52:44 -0600

    ADD: componente para modal

---
+++ resources/js/components/ModalComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="show" class="modal fade show d-block" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+
+        <!-- HEADER -->
+        <div class="modal-header">
+          <h5 class="modal-title">
+            <slot name="title"></slot>
+          </h5>
+          <button type="button" class="close" @click="$emit('close')">
+            <span>&times;</span>
+          </button>
+        </div>
+
+        <!-- BODY -->
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+
+        <!-- FOOTER -->
+        <div class="modal-footer d-flex justify-content-between align-items-center">
+          <slot name="footer"></slot>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    defineProps({
+    show: { type: Boolean, default: false }
+    })
+
+    defineEmits(['close'])
+</script>
